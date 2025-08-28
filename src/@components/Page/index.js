a6e2c6de1eb8d339cd5d3a5a07fdcbd9f541fd98
Jenna (HopeTS) <jennabaudelaire@pm.me>
2025-08-28 02:28:00 -0400

    images

--- src/@components/Page/index.js
+++ src/@components/Page/index.js
@@ -1,5 +1,9 @@
-export const Page = ({ children }) => {
-  return <div className="Page">{children}</div>;
+import clsx from "clsx";
+
+import styles from "./index.module.css";
+
+export const Page = ({ className, children }) => {
+  return <div className={clsx(className, styles.Page)}>{children}</div>;
 };
 
 export default Page;
