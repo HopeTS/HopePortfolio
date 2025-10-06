8e3f06f94c133deddabca60e7fe1ee708d181360
Jenna (HopeTS) <jennabaudelaire@pm.me>
2025-10-06 15:46:33 -0400

    add content, footer transition

--- src/@components/Content/Inner/index.js
+++ src/@components/Content/Inner/index.js
@@ -1,7 +1,9 @@
 import clsx from "clsx";
 
+import styles from "./index.module.css";
+
 export const Inner = ({ children, className }) => {
-  return <div className={clsx("InnerContent", className)}>{children}</div>;
+  return <div className={clsx(styles.Inner, className)}>{children}</div>;
 };
 
 export default Inner;
--- src/@pages/Home/index.js
+++ src/@pages/Home/index.js
@@ -13,26 +13,63 @@ for (const key in data.XP) {
 
 export const Home = () => {
   return (
-    <C.Page>
+    <C.Page className={styles.Home}>
       {/* Hero landing */}
       <C.Content.Outer>
         <C.AnimatedBackground fadeDown={true} />
         <C.Content.Outer className={styles.Home_splash}>
           <C.Content.Inner className={styles.Home_splash_content}>
-            <div className={styles.Home_splash_name}>
-              Jenna <span>[HopeTS]</span>
+            <div className={styles.Home_splash_pfp_wrapper}>
+              <img
+                src={assets.pfp}
+                className={styles.Home_splash_pfp}
+                alt="Selfie of Jenna"
+              />
             </div>
-            <div className={styles.Home_splash_tagline}>
-              Full-Stack Developer
+
+            <div className={styles.Home_splash_text}>
+              <div className={styles.Home_splash_name}>
+                Jenna <span>[HopeTS]</span>
+              </div>
+              <div className={styles.Home_splash_tagline}>
+                Full-Stack Developer
+              </div>
             </div>
+          </C.Content.Inner>
+          <C.Content.Inner className={styles.Home_splash_bottom}>
             <C.LinkRow />
+
+            <div className={styles.Home_bottom_text}>
+              <p>
+                Currently maintaining{" "}
+                <a href="https://github.com/peardrive/PearDriveCore">
+                  PearDrive Core
+                </a>
+              </p>
+
+              <p>
+                <span>Full-stack developer</span>, specializing in{" "}
+                <span>Node.js</span> and <span>React</span> applications.
+              </p>
+
+              <p>
+                Around 5 years of professional experience, and a BSc in Computer
+                Science.
+              </p>
+
+              <p>
+                I love building modules and applications that make the internet
+                more accessible and giving users more control over their digital
+                lives.
+              </p>
+            </div>
           </C.Content.Inner>
         </C.Content.Outer>
       </C.Content.Outer>
 
       {/* Transition between hero and info*/}
       <C.Content.Outer className={styles.Home_transition}>
-        <div className={styles.Home_transition_block} />
+        <div className={styles.Home_transition_block_top} />
       </C.Content.Outer>
 
       {/* Timeline section */}
@@ -40,10 +77,9 @@ export const Home = () => {
         <C.Timeline items={xp} />
       </C.Content.Outer>
 
-      {/* Info section */}
-      <C.Content.Outer className={styles.Home_info}>
-        Home page <C.ThemeToggler />
-        <img src={assets.pfp} className={styles.Home_splash_pfp} />
+      {/* Footer transition */}
+      <C.Content.Outer className={styles.Home_transition_bottom}>
+        <div className={styles.Home_transition_block_bottom} />
       </C.Content.Outer>
     </C.Page>
   );
