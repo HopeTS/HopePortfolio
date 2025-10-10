9e56b971a81db4fdbf8dd691f96999d37aea4c76
Jenna (HopeTS) <jennabaudelaire@pm.me>
2025-10-10 05:06:34 -0400

    fix dates for ios compat, fix mobile sizing pfp

--- src/@data/experience.js
+++ src/@data/experience.js
@@ -3,8 +3,8 @@ import CATEGORY from "./category";
 
 /** Castamatch project */
 export const CASTAMATCH = {
-  startDate: new Date("07-01-2020"),
-  endDate: new Date("05-02-2021"),
+  startDate: new Date(2020, 6, 1),
+  endDate: new Date(2021, 4, 2),
   title: "Castamatch",
   shortDesc: "Livestreaming platform for amateur sports leagues",
   longDesc: "TODO",
@@ -15,8 +15,8 @@ export const CASTAMATCH = {
 
 /** The Web Design Hub internship  */
 export const WEB_DESIGN_HUB = {
-  startDate: new Date("10-01-2018"),
-  endDate: new Date("05-01-2019"),
+  startDate: new Date(2018, 9, 1),
+  endDate: new Date(2019, 4, 1),
   title: "The Web Design Hub",
   shortDesc: "Web development internship",
   longDesc: "TODO",
@@ -35,8 +35,8 @@ export const WEB_DESIGN_HUB = {
 
 /** Finalsite job */
 export const FINALSITE = {
-  startDate: new Date("07-01-2019"),
-  endDate: new Date("12-01-2019"),
+  startDate: new Date(2019, 6, 1),
+  endDate: new Date(2019, 11, 1),
   title: "Finalsite",
   shortDesc: "Web development for educational institutions",
   longDesc: "TODO",
@@ -66,8 +66,8 @@ export const WALKOFF = {
 
 /** Visionable job */
 export const VISIONABLE = {
-  startDate: new Date("08-01-2021"),
-  endDate: new Date("11-01-2022"),
+  startDate: new Date(2021, 7, 1),
+  endDate: new Date(2022, 10, 1),
   title: "Visionable Global",
   shortDesc: "Healthcare-focused telehealth platform",
   longDesc: "TODO",
@@ -85,7 +85,7 @@ export const VISIONABLE = {
 
 /** Anjou (PearDrive) job */
 export const ANJOU = {
-  startDate: new Date("06-01-2023"),
+  startDate: new Date(2023, 5, 1),
   endDate: undefined,
   title: "Anjou (PearDrive)",
   shortDesc: "P2P Cloud storage and file sharing platform",
@@ -104,8 +104,8 @@ export const ANJOU = {
 
 /** ECSU */
 export const ECSU = {
-  startDate: new Date("06-01-2015"),
-  endDate: new Date("05-01-2020"),
+  startDate: new Date(2015, 5, 1),
+  endDate: new Date(2020, 4, 1),
   title: "Eastern Connecticut State University",
   shortDesc: "B.S. Computer Science",
   longDesc: "TODO",
